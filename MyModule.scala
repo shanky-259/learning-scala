45181c1a7f1a58a47cc69d1fab4afcbcef18bec9
Shashank Singh <shashanks@twitter.com>
2018-07-05 15:53:34 -0700

    Adding higher order function formatResult ti implement abs and factorial together

--- MyModule.scala
+++ MyModule.scala
@@ -1,17 +1,42 @@
 /** Scala Redbook : Chapter 2 */
 
+/** Here, formatResult is a higher order function that takes another
+ * function called f.
+ * Also, Int => Int indicates that f expects an integer argument
+ * and will also return an integer.*/
+
 object MyModule {
-  def abs(n : Int) : Int = {
-    if(n < 0) -n
+  def abs(n: Int): Int = {
+    if (n < 0) -n
     else n
   }
 
-  def formatAbs(x : Int) : String = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
+  /** private def formatAbs(x : Int){
+   * val msg = "The absolute value of %d is %d"
+   *msg.format(x, abs(x))
+   * } */
+
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, accumulate: Int): Int = {
+      if (n <= 0) accumulate
+      else go(n - 1, n * accumulate)
+    }
+
+    go(n, 1)
   }
 
-  def main(args : Array[String]) : Unit = {
-    println(formatAbs(-42))
+  /** private def formatFactorial(n: Int){
+   * val msg = "The factorial of %d is %d"
+   *msg.format(x, factorial(x))
+   * } */
+
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d"
+    msg.format(name, n, f(n))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(formatResult("factorial", 5, factorial))
   }
 }
